6c4dbada16784202bae0fa698515a805ee0252d4
Soumodip Chandra <soumodip.chandra.750@gmail.com>
2024-02-10 22:06:16 +0530

    ui change

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,3 +16,22 @@ module.exports = {
     // ...
   ],
 }
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        'border-color-change': 'border-color-change 2s infinite',
+      },
+      keyframes: {
+        'border-color-change': {
+          '0%': { borderColor: '#ff0000' },
+          '33%': { borderColor: '#00ff00' },
+          '66%': { borderColor: '#0000ff' },
+          '100%': { borderColor: '#ff0000' },
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
